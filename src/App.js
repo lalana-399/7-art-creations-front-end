6acddf650e18c5f726e6a103b09e46676a09e9cf
Lalana <s16399@sci.pdn.ac.lk>
2020-09-30 23:05:51 +0530

    enter the first code

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Navbar from './component/Navbar/Navbar';
+
+function App() {
+  return (
+  
+    <div className="App">
+      <Navbar/>
+      </div>
+
+
+      // <img src ="/images/bridal.jpg" alt=""/>
+      // <div className='container'>
+      //   <h1>hello world</h1>
+        
+      // </div>
+      // <div className='container'>
+      // <div className="jumbotron">
+      //   <h1>todo app</h1>
+      //   <h2>this is my first bootstrap project</h2>
+      // </div>
+      // </div>
+   
+  );
+}
+
+export default App;
---
+++ src/component/Navbar/Manuitem.js
@@ -0,0 +1,30 @@
+//  export const Menuitems =[
+// {
+//     title:'home',
+//     url:'#',
+//     cName: 'nav-links'
+// },
+// {
+//     title:'servisers',
+//     url:'#',
+//     cName: 'nav-links'
+// },
+// {
+//     title:'products',
+//     url:'#',
+//     cName: 'nav-links'
+// },
+// {
+//     title:'contact-us',
+//     url:'#',
+//     cName: 'nav-links'
+// },
+// {
+//     title:'sign up',
+//     url:'#',
+//     cName: 'nav-links-mobile'
+// },
+   
+    
+
+// ]
